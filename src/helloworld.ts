340721612b22e66abda24d670ee2cebb929e6ecc
LongZhou <zhlo@ip-192-168-0-200.ec2.internal>
2024-08-19 14:14:01 -0400

    add new func

--- src/helloworld.ts
+++ src/helloworld.ts
@@ -2,4 +2,7 @@ export class HelloWorld {
     sayHello (name: string = 'World') {
         console.log(`Hello, ${name}`)
     }
+    sayHello2 (name: string = 'World') {
+        console.log(`Hello world, ${name}`)
+    }
 }
